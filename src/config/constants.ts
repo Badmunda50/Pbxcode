c320f9cfbbd97b23730eb7e7378e74b223c18821
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-04-16 11:14:10 +0530

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const FOOTER_MESSAGE = `<blockquote>🛠 Developed by Binamra Lamsal\n📢 Official Channel for announcements: @WordSeek\n🗨 Official Group for playing and suggestions: @WordGuesser</blockquote>`;
+export const FOOTER_MESSAGE = `<blockquote>📢 Official Channel for announcements: @HEROKUBIN_01\n🗨 Official Group for playing and suggestions: @PBX_CHAT \n 💸My Developer @ll_BAD_MUNDA_ll ❤️ </blockquote>`;
 
 export const allowedChatSearchKeys = ["global", "group"] as const;
 export const allowedChatTimeKeys = [
