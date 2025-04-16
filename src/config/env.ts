b9d7f9b5eb2e6f0319605f743ea7c7e59470b9fe
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-04-16 13:36:59 +0530

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,6 @@ import { z } from "zod";
 export const env = z
   .object({
     BOT_TOKEN: z.string().min(1, { message: "BOT_TOKEN is required" }),
-    DATABASE_URI: z.string().min(1, { message: "DATABASE_URI is required" }),
     NODE_ENV: z.enum(["development", "production"]).default("development"),
     ADMIN_USERS: z
       .string()
