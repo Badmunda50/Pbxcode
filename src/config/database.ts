67f35a3c235d3cb7f5222fb1e726da75fbcbabf0
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-04-16 23:50:34 +0530

    Delete src/config/database.ts

--- src/config/database.ts
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from "mongodb";
-import { config } from "dotenv";
-
-config(); // Ensure environment variables are loaded
-
-const client = new MongoClient(process.env.DATABASE_URI!);
-
-export const db = client.db("your-database-name"); // Replace "your-database-name" with your actual database name
