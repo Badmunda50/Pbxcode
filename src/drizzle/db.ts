a4360cfc9c6ffbed53c90cde89d99a2b91c89672
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-04-16 14:56:40 +0530

    Update db.ts

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,4 +1,5 @@
-import { drizzle } from "drizzle-orm/mongodb";
+//import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
 
 import { env } from "../config/env";
 import * as schema from "./schema";
