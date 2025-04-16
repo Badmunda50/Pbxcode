29170c789be28c8b6c71af6b865099fb65b76e74
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-04-16 23:05:49 +0530

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,6 @@
+import { config } from "dotenv";
+config();
+
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(env.DATABASE_URI);
-
-export const db = client.db();
+const client = new MongoClient(process.env.DATABASE_URI!); // Use process.env to access environment variables
