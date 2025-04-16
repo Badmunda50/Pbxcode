bdaaff74d0088960ad9972fb39fa08c28c9f9dd5
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-04-16 13:46:05 +0530

    Update db.ts

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,9 +1,10 @@
-import { drizzle as drizzleSQLite } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
+//import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import { env } from "../config/env";
 import * as schema from "./schema";
 
-const sqliteDB = new Database("sqlite-database.db");
-export const db = drizzleSQLite(sqliteDB, {
+export const db = drizzle(env.DATABASE_URI, {
   schema,
-  logger: process.env.NODE_ENV === "development",
+  logger: env.NODE_ENV === "development",
 });
