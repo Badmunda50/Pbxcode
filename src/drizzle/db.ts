035439a5443e8251517ff09b746cd88dafe991f9
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-04-21 10:12:30 +0530

    Update db.ts

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,10 +1,14 @@
-//import { drizzle } from "drizzle-orm/neon-http";
-import { drizzle } from "drizzle-orm/node-postgres";
-
+import mongoose from "mongoose";
 import { env } from "../config/env";
-import * as schema from "./schema";
 
-export const db = drizzle(env.DATABASE_URI, {
-  schema,
-  logger: env.NODE_ENV === "development",
-});
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(env.DATABASE_URI);
+    if (env.NODE_ENV === "development") {
+      console.log("MongoDB connected");
+    }
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
